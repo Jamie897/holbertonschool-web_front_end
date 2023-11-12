3ee572a8d9596a4fd7b1de9c5d4d1f89c4e67202
Jamie897 <jamiedanielle456@gmail.com>
2023-11-12 10:11:14 -0600

    Student Seat

---
+++ 3-classrooms.js
@@ -0,0 +1,14 @@
+function createClassroom(numbersOfStudents) {
+    function studentSeat(seat);
+    return(seat);
+
+}
+
+let students = [];
+    for (let i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+
+const classRoom = createClassRoom(10);
