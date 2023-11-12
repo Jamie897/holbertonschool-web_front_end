dcaed8dfedc14e5829b909afeed346e9d61e8db8
Jamie897 <jamiedanielle456@gmail.com>
2023-11-12 13:35:45 -0600

    Room area

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+var roomDimensions; {
+    width: 50;
+    length: 100;
+}
+
+function getArea() {
+    return roomDimensions.width * roomDimensions.length }
+
+    var boundGetArea = getArea.bind(roomDimensions);
+    boundGetArea()
