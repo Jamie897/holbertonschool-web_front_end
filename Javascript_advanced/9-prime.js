4916a83dec1050082266ab272525a16ecfb222b1
Jamie897 <jamiedanielle456@gmail.com>
2023-11-12 12:38:24 -0600

    Stopping point

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,33 +1,6 @@
-// Prime numbers & timing execution
+const countPrimeNumbers() {
+    const startTime = performance.now();
 
-function countPrimeNumbers(){
-    let numOfPrimeNums = 0
-
-    function isPrime(number) {
-        status = 1;
-        if (number < 2) {
-            status = 0;
-        }
-        else {
-            for (let i = 2; i < number; i++) {
-                if (number % i === 0) {
-                    status = 0;
-                }
-            }
-        }
-        return status
-    }
-
-    for (let num = 2; num < 101; num++) {
-        if (isPrime(num) == 1) {
-            numOfPrimeNums += 1
-        }
-    }
-
-    return numOfPrimeNums;
+    const isPrime = num;
+    if (num)
 }
-
-let a = performance.now();
-countPrimeNumbers();
-let b = performance.now();
-console.log('Execution time of printing countPrimeNumbers was ' + (b - a) + ' milliseconds.');
